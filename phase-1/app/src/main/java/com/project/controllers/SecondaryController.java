92f62f36c0780db593ef2dfa86ede8501a437a9a
Theborna <borna710@gmail.com>
2022-07-17 23:37:49 +0430

    added le page view

--- phase-1/app/src/main/java/com/project/controllers/SecondaryController.java
+++ phase-1/app/src/main/java/com/project/controllers/SecondaryController.java
@@ -1,9 +1,12 @@
 package com.project.controllers;
 
 import com.project.App;
+import com.project.models.node.user.User;
 import com.project.view.general.ChatListView;
 import com.project.view.general.CreatePostView;
 import com.project.view.general.FeedView;
+import com.project.view.model.PageView;
+
 import static com.project.util.StdOut.*;
 
 public class SecondaryController implements Controller {
@@ -17,6 +20,9 @@ public class SecondaryController implements Controller {
                 break;
             case "chat":
                 App.setView(ChatListView.getInstance());
+                break;
+            case "page":
+                App.setView(PageView.getInstance().setUser(User.getCurrentUser()));
                 break;
             case "help":
                 help();
