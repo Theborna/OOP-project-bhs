8fd8001101bd3073021338e5684b2da9baea8f28
Theborna <borna710@gmail.com>
2022-07-19 19:02:40 +0430

    added kos and sher to register

--- phase-1/app/src/main/java/com/project/AppRegex.java
+++ phase-1/app/src/main/java/com/project/AppRegex.java
@@ -7,8 +7,11 @@ public enum AppRegex {
     USERNAME("\\w+"), PASSWORD("\\w+"), SWITCH("switch to -(?<next>\\w+)"),
     POST_FROM_DB(
             "%id% = (?<id>\\d+) %text% = (?<text>\\.+) %sender_id% = (?<sender_id>\\d+) %likes% = (?<likes>\\d+) %views% = (?<views>\\d+)"),
-            ;
+    DATE(" ^((?:(?:1[6-9]|2[0-9])\\d{2})(-)(?:(?:(?:0?[13578]|1[02])(-)31)|((0?[1,3-9]|1[0-2])(-)(29|30))))$|^(?:(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00)))(-)0?2(-)29)$|^(?:(?:1[6-9]|2[0-9])\\d{2})(-)(?:(?:0?[1-9])|(?:1[0-2]))(-)(?:0?[1-9]|1\\d|2[0-8])$"),
+    // mother of all regex
     
+    ;
+
     String regex;
 
     AppRegex(String regex) {
@@ -20,5 +23,9 @@ public enum AppRegex {
         if (m.matches())
             return m;
         return null;
+    }
+
+    public boolean matches(String input) {
+        return input.matches(this.regex);
     }
 }
--- phase-1/app/src/main/java/com/project/controllers/RegisterController.java
+++ phase-1/app/src/main/java/com/project/controllers/RegisterController.java
@@ -1,9 +1,37 @@
 package com.project.controllers;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import com.project.AppRegex;
+
 public class RegisterController extends LoginController {
 
     @Override
     public void parse(String input) {
+    }
+
+    public String getRepeatPassword(String nextLine, String password) {
+        if (nextLine.equals(password))
+            return password;
+        return null;
+    }
+
+    public String getFullName(String nextLine) {
+        if (nextLine.matches("\\S+") && nextLine.length() <= 30)
+            return nextLine;
+        return null;
+    }
+
+    public java.util.Date getBirthDate(String input) {
+        if (!AppRegex.DATE.matches(input))
+            return null;
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            return format.parse(input);
+        } catch (ParseException e) {
+            return null;
+        }
     }
 
 }
