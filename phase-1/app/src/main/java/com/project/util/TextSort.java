662fa11d8ecf2d75bc2bcccd7e5a23a69e2bdba4
Theborna <borna710@gmail.com>
2022-07-22 15:22:25 +0430

    watch for null!

--- phase-1/app/src/main/java/com/project/util/TextSort.java
+++ phase-1/app/src/main/java/com/project/util/TextSort.java
@@ -22,6 +22,10 @@ public class TextSort<E extends node & TextBased> implements Comparator<E> {
 
     @Override
     public int compare(E arg0, E arg1) { // only care about positive values
+        if (arg0.getText() == null)
+            return -1;
+        if (arg1.getText() == null)
+            return -1;
         if (!contains(arg0.getText(), arg1.getText()))
             return -1;
         return arg0.getLastModifiedDate().compareTo(arg1.getLastModifiedDate());
