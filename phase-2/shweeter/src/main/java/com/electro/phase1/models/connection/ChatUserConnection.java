71a94dd00c7ebc45a3a4ecbc569e8b62deadbfeb
Theborna <borna710@gmail.com>
2022-08-06 23:48:11 +0430

    oopsie

--- phase-2/shweeter/src/main/java/com/electro/phase1/models/connection/ChatUserConnection.java
+++ phase-2/shweeter/src/main/java/com/electro/phase1/models/connection/ChatUserConnection.java
@@ -23,14 +23,7 @@ public class ChatUserConnection extends connection<User, Chat> {
 
     public static Set<Chat> getChats(Long userId) {
         Set<Chat> result = new LinkedHashSet<Chat>();
-        // TODO: run a query and get all the chats the user is connected to
-        // result.add(new Chat("sepronites", ChatType.GROUP));
-        // result.add(new Chat("borna saving memes", ChatType.CHANNEL));
-        // result.add(new Chat("oop", ChatType.GROUP));
-        // result.add(new Chat("sepehr", ChatType.PRIVATE));
-        // for (int i = 0; i < 10; i++) {
-        // result.add(new Chat(String.valueOf(i), ChatType.PRIVATE));
-        // }
+
         try {
             result.addAll(ChatDB.getChats(userId));
         } catch (SQLException e) {
@@ -39,16 +32,13 @@ public class ChatUserConnection extends connection<User, Chat> {
         return result;
     }
 
-    // TODO: i made this into a Map!!!
-    public static Map<User, ChatPermission> getUsers(Long chatId) { // TODO: i made this into a Map!!!
+    public static Map<User, ChatPermission> getUsers(Long chatId) {
         Map<User, ChatPermission> result = new LinkedHashMap<User, ChatPermission>();
-        // TODO: run a query and get all the users in the chat
-        // result.put(new NormalUser("sex", "anal"), ChatPermission.NORMAL);
-        // result.put(new NormalUser("sex2", "vaginal"), ChatPermission.NORMAL);
-        // for (int i = 0; i < 10; i++) {
-        //     result.put(new NormalUser(Integer.toString(i), Integer.toString((2 * i))), ChatPermission.NORMAL);
-        // }
-        // UserDB.get
+        try {
+            result = ChatDB.getMembersOfChat(chatId);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return result;
     }
 }
