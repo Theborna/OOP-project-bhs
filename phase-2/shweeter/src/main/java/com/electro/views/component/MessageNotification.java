386a19ad7d8e14ac0f7377cdef53f41b09043b06
Theborna <borna710@gmail.com>
2022-08-08 00:35:08 +0430

    working notifs

--- phase-2/shweeter/src/main/java/com/electro/views/component/MessageNotification.java
+++ phase-2/shweeter/src/main/java/com/electro/views/component/MessageNotification.java
@@ -8,6 +8,7 @@ import com.electro.phase1.models.connection.MessageConnection;
 import com.electro.phase1.models.node.Chat;
 import com.electro.phase1.models.node.Message;
 
+import javafx.application.Platform;
 import javafx.geometry.Pos;
 import javafx.util.Duration;
 import jfxtras.styles.jmetro.Style;
@@ -25,15 +26,17 @@ public class MessageNotification {
         Message msg = MessageConnection.getLastMessage(chatId);
         if (msg == null)
             return;
-        Notifications notification = Notifications.create()
-                .title(msg.getCh().getName())
-                .text(msg.getAuthor().getName() + ": " + msg.getText())
-                .graphic(null)// TODO
-                .hideAfter(Duration.seconds(2))
-                .position(Pos.TOP_RIGHT);
-        if (App.getStyle() == Style.DARK)
-            notification.darkStyle();
-        notification.show();
+        Platform.runLater(() -> {
+            Notifications notification = Notifications.create()
+                    .title(msg.getCh().getName())
+                    .text(msg.getAuthor().getName() + ": " + msg.getText())
+                    .graphic(null)// TODO
+                    .hideAfter(Duration.seconds(2))
+                    .position(Pos.TOP_RIGHT);
+            if (App.getStyle() == Style.DARK)
+                notification.darkStyle();
+            notification.show();
+        });
         Thread th = new Thread(() -> {
             try {
                 Thread.sleep(2000);
