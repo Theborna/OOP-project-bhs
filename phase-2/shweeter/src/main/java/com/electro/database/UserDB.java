a7a464d763d206360a2bb8145159c01f2c115d74
hanjidani <hoseinanjidani2@gmail.com>
2022-08-09 17:50:25 +0430

    ublock

--- phase-2/shweeter/src/main/java/com/electro/database/UserDB.java
+++ phase-2/shweeter/src/main/java/com/electro/database/UserDB.java
@@ -303,15 +303,23 @@ public class UserDB {
         Connection con = getConnection();
         Statement st = con.createStatement();
         ResultSet rs = st.executeQuery(
-                "select * from block where user_id = " + userID);
+                "select * from block where blocked_id = " + userID);
         ArrayList<Long> ret = new ArrayList<>();
         while (rs.next()) {
-            ret.add(rs.getLong(2))
+            ret.add(rs.getLong(1));
         }
         rs.close();
         st.close();
         con.close();
         return ret;
+    }
+
+    public static void Unblock(long currentUser, long touBlock) throws SQLException {
+        Connection con = getConnection();
+        String query = "delete from block where user_id = " + currentUser + " and blocked_id = " + touBlock;// TODO: add pfp
+        // System.out.println(query);
+        con.createStatement().execute(query);
+        con.close();
     }
 
 }
